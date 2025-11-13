f424a643f4587e41791408c13504bdef36c88a10
jonction-project <jonctionpro@gmail.com>
2025-11-12 20:19:05 -0600

    First commit fun project

---
+++ pdf_merger.py
@@ -0,0 +1,14 @@
+import pypdf
+import sys
+
+inputs = sys.argv[1:]
+
+
+def pdf_merger(pdf_list):
+    merger = pypdf.PdfWriter()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write('merged.pdf')
+
+
+pdf_merger(inputs)
